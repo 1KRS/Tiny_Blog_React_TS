f15a89cd7fcb50150a53f5a7504b6b5995432f76
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-01-18 13:10:48 +0200

    CSS tweeks

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -19,7 +19,6 @@ const List = ( {tag}: ITag ) => {
   useEffect(() => {
     axios.get('https://dummyjson.com/posts')
     .then(res => {
-      console.log('res', res);
       setData({
         posts: res.data.posts,
         total: res.data.total,
@@ -31,15 +30,16 @@ const List = ( {tag}: ITag ) => {
   }, [])
 
   const articles = data.posts.filter(a => a.tags.includes(tag.toLowerCase()))
-
+  console.log(`Articles for ${tag}`, articles);
   return (
     <div className='accordion'>
             {articles.map((article, i) => (
-              <article className='article'>
+              <article className='article' key={i}>
                 <h4>{article.title}</h4>
-                  <div>
-                    {article.body}
-                  </div>
+                <div>
+                  {article.body}
+                </div>
+                
                 {/* <div className='section-list'>
                   <Article />
                 </div> */}
